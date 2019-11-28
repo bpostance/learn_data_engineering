288057d65412f9ca9f1a9edb68d6af4b960275b8
pen-b <b.postance@lboro.ac.uk>
2019-11-28 18:20:35 +0000

    update: add src for gdrive download

--- docker-compose/manager/manager-py/gdrive.sh
+++ docker-compose/manager/manager-py/gdrive.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# method: https://gist.github.com/tanaikech/f0f2d122e05bf5f971611258c22c110f#update-at-march-21-2019
 fileid="1pqZvEIMKehdoiCNaob2HutHTdvCbtLub"
 filename="wine-reviews.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
