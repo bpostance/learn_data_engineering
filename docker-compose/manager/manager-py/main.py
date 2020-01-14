61cde9360169887e9eb49536549989b1e5adfe1e
bpostance <benpostance@gmail.com>
2020-01-14 20:00:41 +0000

    update: compose

--- docker-compose/manager/manager-py/main.py
+++ docker-compose/manager/manager-py/main.py
@@ -17,27 +17,30 @@ def connect_to_postgres(server_string,db_name):
     connection = engine.connect()
     return connection
 
-# create db on server
-server_string = "postgresql+psycopg2://postgres@pgdb:5432"
-db_name = 'db'
-
-conn = connect_to_postgres(server_string,'')
-conn.execute("commit")
-
 try:
-    conn.execute(f"create database {db_name}")
-except:
-    pass
-conn.close()
+    # create db on server
+    server_string = "postgresql+psycopg2://postgres@wine-postgres:5432"
+    db_name = 'db'
 
-# read data 
-df = pd.read_csv('/home/working-dir/data/wine-reviews/winemag-data-130k-v2.csv',index_col=0)
+    conn = connect_to_postgres(server_string,'')
+    conn.execute("commit")
 
-# push data to db
-df.to_sql(name='wine_reviews',
+    try:
+        conn.execute(f"create database {db_name}")
+    except:
+        pass
+    conn.close()
+
+    # read data 
+    df = pd.read_csv('/home/working-dir/data/wine-reviews/winemag-data-130k-v2.csv',index_col=0)
+
+    # push data to db
+    df.to_sql(name='wine_reviews',
           con=connect_to_postgres(server_string,db_name),
           index=False,
           if_exists='replace',
           chunksize=5000)
 
-print('Upload Data Done')
+    print('Upload Data Done')
+except:
+    print('db fail')
