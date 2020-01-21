4168fed6b5be6a068d5367f1e8d428d5e5405379
pen-b <b.postance@lboro.ac.uk>
2020-01-21 19:36:14 +0000

    update: db utils

--- docker-compose/manager/manager-py/main.py
+++ docker-compose/manager/manager-py/main.py
@@ -11,43 +11,37 @@ from sqlalchemy import create_engine
 print('manager.py is running')
 print(f'cwd: {os.getcwd()}')
 
-
 def connect_to_postgres(server_string,db_name):
     """connect to postgres"""
     engine = create_engine('{}/{}'.format(server_string,db_name))
-    connection = engine.connect()
-    return connection
+    return engine
 
-try:
+def create_db(db_name):
     # create db on server
+    try:
+        engine = connect_to_postgres(server_string,'')
+        connection = engine.connect()
+        conn.execute("commit")
+        conn.execute(f"create database {db_name}")
+    except:
+        print(f'db exists: {db_name}')
+    conn.close()
+    engine.dispose()
+
+def upload_data(db_name):
     server_string = "postgresql+psycopg2://postgres:postgres@192.168.1.156:5433"
-    db_name = 'db'
-
-    #conn = connect_to_postgres(server_string,'')
-    #conn.execute("commit")
-
-    #try:
-    #    conn.execute(f"create database {db_name}")
-    #except:
-    #    pass
-    #conn.close()
+    engine = connect_to_postgres(server_string,db_name)
+    conn = engine.connect()
 
     # read data 
     df = pd.read_csv('/home/working-dir/data/wine-reviews/winemag-data-130k-v2.csv',index_col=0)
 
-    for attempt in [1,2,3]:
-        try:
-            # push data to db
-            df.to_sql(name='wine_reviews',
-                      con=connect_to_postgres(server_string,db_name),
-                      index=False,
-                      if_exists='replace',
-                      chunksize=5000)
-        except:
-            time.sleep(60)
-            print(f'try {attempt}: sleeping')
+    # push data to db
+    df.to_sql(name='wine_reviews',
+                con=conn,
+                index=False,
+                if_exists='replace',
+                chunksize=5000)
 
-    print('Upload Data Done')
-except Exception as error:
-    print(error)
-    print('\n\ndb fail')
+    conn.close()
+    engine.dispose()
